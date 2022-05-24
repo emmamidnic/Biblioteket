a073418979272f6652994cb6a9d2c4b8cd412b70
emmamidnic <email@example.com>
2022-05-24 13:40:30 +0200

    kontrol

---
+++ Biblioteket/Bibliotek.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteket
+{
+    class Bibliotek
+    {
+
+        private string _biblioteksNavn;
+
+        public Bibliotek(string biblioteksNavn)
+        {
+            _biblioteksNavn = biblioteksNavn;
+        }
+
+        public string HentBibliotek()
+        {
+            
+            return string.Format("Velkommen til {0} - Datoen i dag er: {1}", _biblioteksNavn, DateTime.Now);
+        }
+
+
+    }
+}
